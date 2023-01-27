6e43fe89856b44ec552c49ad4b743172a1371b13
Flora-0707 <florawang.smile@gmail.com>
2023-01-26 19:58:44 -0800

    Implement combine function

--- pmg_csv_combiner/main.py
+++ pmg_csv_combiner/main.py
@@ -1,10 +1,46 @@
+import csv
+import os
+import sys
+from contextlib import ExitStack
+
 import click
 
+
+CSV_COLUMN_FILENAME = "filename"
+
+
+def combine(srcs: tuple[str]) -> None:
+    with ExitStack() as stack:
+        fds = [stack.enter_context(open(src, newline='')) for src in srcs]
+        readers = [csv.DictReader(fd, doublequote=False, escapechar='\\') for fd in fds]
+
+        columns = []
+        for reader in readers:
+            columns += reader.fieldnames
+        columns = list(dict.fromkeys(columns))
+        columns.append(CSV_COLUMN_FILENAME)
+
+        writer = csv.DictWriter(
+            sys.stdout,
+            fieldnames=columns,
+            restval='',
+            doublequote=False,
+            escapechar='\\', 
+            quoting=csv.QUOTE_ALL,
+        )
+        writer.writeheader()
+
+        for src, reader in zip(srcs, readers):
+            for row in reader:
+                row[CSV_COLUMN_FILENAME] = os.path.basename(src)
+                writer.writerow(row)
+
+
 @click.command()
-@click.argument("src", nargs=-1)
-@click.argument("dst", nargs=1)
-def main(src: tuple[str], dst: str):
-    print(src, dst)
+@click.argument("srcs", nargs=-1)
+def main(srcs: tuple[str]) -> None:
+    combine(srcs)
+
 
 if __name__ == "__main__":
     main()
